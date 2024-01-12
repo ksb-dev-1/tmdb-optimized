7a94f80abf30cfe6d89bd387da040a6ccef9df09
ksb-dev-1 <ksbabaleshwar@gmail.com>
2024-01-12 23:59:31 +0530

    Initial Commit

--- src/app/pages/watchlist/movies/page.tsx
+++ src/app/pages/watchlist/movies/page.tsx
@@ -1,34 +1,5 @@
-import { db } from "@/db";
-import { auth } from "@/auth";
 import Watchlist from "@/components/Watchlist/Watchlist";
-import { redirect } from "next/navigation";
-
-const getWatchlist = async (): Promise<Watchlist[]> => {
-  const session = await auth();
-  let watchlist;
-  try {
-    watchlist = await db.watchlist.findMany({
-      where: {
-        mediaType: "movie",
-        userId: session?.user?.id,
-      },
-    });
-    if (!watchlist) {
-      throw new Error(`Failed to fetch movies watchlist! 404`);
-    }
-  } catch (error) {
-    console.log(error);
-  }
-  return watchlist!;
-};
 
 export default async function WatchlistMoviesPage() {
-  const session = await auth();
-  const watchlist = await getWatchlist();
-
-  return (
-    <>
-      {session?.user?.id ? <Watchlist watchlist={watchlist} /> : redirect("/")}
-    </>
-  );
+  return <Watchlist category="movie" />;
 }
--- src/components/Watchlist/Watchlist.tsx
+++ src/components/Watchlist/Watchlist.tsx
@@ -5,11 +5,43 @@ import WatchlistCategories from "@/components/Watchlist/WatchlistCategories";
 import WatchlistBackdrop from "@/components/Watchlist/WatchlistBackdrop";
 import Link from "next/link";
 import ConditionalSuspenseFallback from "./ConditionalSuspenseFallback";
+import { db } from "@/db";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
-export default function Watchlist({ watchlist }: { watchlist: Watchlist[] }) {
+const getWatchlist = async (category: string): Promise<Watchlist[]> => {
+  const session = await auth();
+  let watchlist;
+  try {
+    if (category === "watchlist") {
+      watchlist = await db.watchlist.findMany({
+        where: {
+          userId: session?.user?.id,
+        },
+      });
+    } else {
+      watchlist = await db.watchlist.findMany({
+        where: {
+          mediaType: category,
+          userId: session?.user?.id,
+        },
+      });
+    }
+    if (!watchlist) {
+      throw new Error(`Failed to fetch watchlist! 404`);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  return watchlist!;
+};
+
+export default async function Watchlist({ category }: { category?: string }) {
+  const session = await auth();
+  const watchlist = await getWatchlist(category!);
   return (
     <>
-      {watchlist.length ? (
+      {session?.user?.id && watchlist.length > 1 && (
         <div className="max-w-[1280px] w-[100%] mx-auto">
           <Suspense fallback={<WatchlistBackdropSkeleton />}>
             <WatchlistBackdrop image={watchlist[0].backdropPath} />
@@ -31,7 +63,9 @@ export default function Watchlist({ watchlist }: { watchlist: Watchlist[] }) {
             </ConditionalSuspenseFallback>
           </div>
         </div>
-      ) : (
+      )}
+
+      {session?.user?.id && watchlist.length === 0 && (
         <div className="flex flex-col items-center justify-center h-[calc(100vh-(188px+64px))] md:h-[calc(100vh-(124px+64px))] px-4">
           <p className="font-semibold text-lg mb-4 text-center">
             Nothing to show here! Please add something to watchlist.
@@ -44,6 +78,8 @@ export default function Watchlist({ watchlist }: { watchlist: Watchlist[] }) {
           </Link>
         </div>
       )}
+
+      {!session?.user?.id && redirect("/")}
     </>
   );
 }
